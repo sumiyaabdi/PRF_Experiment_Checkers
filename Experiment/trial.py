ca35b14c666c8f393e4a02d209d19a0ce0185331
sumiyaabdi <20847637+sumiyaabdi@users.noreply.github.com>
2021-03-25 17:18:31 +0100

    fix scanner sync, screenshots, timing

--- Experiment/trial.py
+++ Experiment/trial.py
@@ -31,7 +31,9 @@ class PRFTrial(Trial):
             #here we decide how to go from each trial (bar position) to the next.
             if self.session.settings['PRF stimulus settings']['Scanner sync']==True:
                 #dummy value: if scanning or simulating a scanner, everything is synced to the output 't' of the scanner
-                self.phase_durations = [100]*self.session.stim_per_trial
+                self.phase_durations = [self.session.settings['PRF stimulus settings']['Bar step length']\
+                                       /(self.session.stim_per_trial*2)]*(self.session.stim_per_trial*2-1)
+                self.phase_durations.append(100) # only last phase needs dummy value
             else:
                 #if not synced to a real or simulated scanner, take the bar pass step as length
                 self.phase_durations = [self.session.settings['PRF stimulus settings']['Bar step length']\
@@ -130,7 +132,8 @@ class PRFTrial(Trial):
                     if key == self.session.mri_trigger:
                         event_type = 'pulse'
                          #marco edit. the second bit is a hack to avoid double-counting of the first t when simulating a scanner
-                        if self.session.settings['PRF stimulus settings']['Scanner sync']==True and t>0.1:
+                        if self.session.settings['PRF stimulus settings']['Scanner sync']==True \
+                                and t>0.1 and self.phase == self.session.settings['attn_task']['stim_per_trial']*2-1:
                             self.exit_phase=True
                              #ideally, for speed, would want  getMovieFrame to be called right after the first winflip.
                              #but this would have to be dun from inside trial.run()
